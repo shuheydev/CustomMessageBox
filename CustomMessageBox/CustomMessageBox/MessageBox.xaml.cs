a180d36ec4254bfe1115ce302138e6b940f11128
shuheydev <shuhey.dev@gmail.com>
2020-03-06 21:01:58 +0900

    Add project files.

---
+++ CustomMessageBox/CustomMessageBox/MessageBox.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomMessageBox
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessageBox : PopupPage
+    {
+        public MessageBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PopPopupAsync(true);
+        }
+    }
+}
